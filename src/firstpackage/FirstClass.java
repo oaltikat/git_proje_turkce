a3264679cb5dafcfa624607746b78447547cce63
oaltikat <oaltikat@gmail.com>
2020-06-20 15:34:26 -0400

    Conflict edecegiz local'den

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
 		System.out.println("Second commit from local repository");
-		System.out.println("Third commit from local repository");
+		System.out.println("Ucuncu commot local repository'den");
 		System.out.println("First commit from remote repository");
 		
 
